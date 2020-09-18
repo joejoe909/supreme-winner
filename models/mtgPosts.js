3c9e569056781a87ba4aa5d60f4fd0aaab117dea
Joseph Farrish <joseph_a_f@hotmail.com>
2020-09-17 19:44:02 -0700

    added model

---
+++ models/mtgPosts.js
@@ -0,0 +1,4 @@
+const Sequelize = requier("sequelize");
+const sqlz = require("../config/")
+
+const mtgPst = 
--- public/js/magic.js
+++ public/js/magic.js
@@ -1,1 +1,17 @@
-console.log("magic.js loaded")
+$(document).ready(function () {
+
+    
+
+
+
+    $("#mkPost").on("click", function () {
+        console.log("mkpost click");
+
+    });
+
+    $("#addCard").on("click", function () {
+        console.log("addCard click");
+
+    });
+    
+});
