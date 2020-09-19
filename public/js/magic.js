061883a94d889b165ff7504231d75caed54c1235
Joseph Farrish <joseph_a_f@hotmail.com>
2020-09-19 10:06:10 -0700

    we are now get good results into our own object we can soon store in the DB

--- public/js/magic.js
+++ public/js/magic.js
@@ -1,24 +1,62 @@
 $(document).ready(function () {
-    let cardName = 'Sol Ring';
-    // send an AJAX POST-request with jQuery
-   
-  
+    let hasCard = false;
+    let mtgCard = Object.create(card);
+    
+    function getCard(cardSrch) {
+       mtgCard.blankCard();
+        $.get("/api/card/" + cardSrch)
+            // on success, run this callback
+            .then(function (data) {
+                // log the data we found
+                console.log(data);
+                mtgCard.name = data[0].name;
+                mtgCard.imageUrl = data[0].imageUrl;
+                mtgCard.type = data[0].type;
+                mtgCard.cmc = data[0].cmc;
+                mtgCard.power = data[0].power;
+                mtgCard.toughness = data[0].toughness;
+                mtgCard.loyalty = data[0].loyalty              
+                console.log(mtgCard);
+                mtgCard.blankCard();
+            });
+            
+    };
 
+
+    // send an AJAX POST-request with jQuery
     $("#mkPost").on("click", function () {
         console.log("mkpost click");
 
     });
 
     $("#addCard").on("click", function () {
+        let cardName = prompt("Type in the Name of your Card.");
+        console.log(cardName);
         console.log("addCard click");
-        $.get("/api/card/"+ cardName)
-        // on success, run this callback
-        .then(function (data) {
-         // log the data we found
-         console.log(data);
-         $("#postBx").val(data);
-        });
+        getCard(cardName);
+       
     });
+
+   
+
    
 });
 
+let card = {
+    name: "",
+    imageUrl: "",
+    type: "",
+    cmc: "",
+    power:"",
+    toughness:"",
+    loyalty:"",
+    blankCard: function(){
+        this.name = "";
+        this.imageUrl = "";
+        this.type = "";
+        this.cmc = "";
+        this.power = "";
+        this.toughness = "";
+        this.loyalty = "";
+    }  
+}
